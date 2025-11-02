367bfc677edf267a3411b5540b40481bf5fd1536
CarlosPerez <carlosmperez2901@gmail.com>
2025-11-02 14:01:05 -0800

    Add route to send new messages

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ import express from 'express';
 import 'dotenv/config';
 import path from 'node:path';
 import appRouter from './routes/index.js';
+import messageRouter from './routes/messageRouter.js';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.set('views', path.join(import.meta.dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', appRouter);
+app.use('/new', messageRouter);
 
 
 app.listen(PORT);
---
+++ routes/messageRouter.js
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { messages } from './index.js';
+
+const messageRouter = Router();
+
+messageRouter.get('', (req, res) => res.render('messageForm'));
+messageRouter.post('', (req, res) => {
+    console.info(req.body);
+    const newMessage = {
+        text: req.body.message,
+        user: req.body.username,
+        added: new Date(),
+    };
+    messages.push(newMessage);
+    res.redirect('/');
+});
+
+export default messageRouter;
