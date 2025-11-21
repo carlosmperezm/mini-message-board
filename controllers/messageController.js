a7ad8758d6fb75ea5c8f6de1f5c1cc271de761f0
CarlosPerez <carlosmperez2901@gmail.com>
2025-11-21 15:39:31 -0800

    Validate and sanitize the data when creating a new message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,16 +1,9 @@
 import * as db from "../db/queries.js";
+import { body, validationResult } from "express-validator";
 
 export async function getAllMessages(req, res) {
     const rows = await db.getAllMessages();
     res.render("index", { messages: rows })
-}
-export async function createMessage(req, res) {
-    const newMessage = {
-        message: req.body.message,
-        username: req.body.username,
-    };
-    await db.saveMessage(newMessage);
-    res.redirect('/');
 }
 export async function getMessageDetails(req, res) {
     const message = { username: req.params.username, message: req.params.message, date: req.params.date };
@@ -19,3 +12,26 @@ export async function getMessageDetails(req, res) {
 export async function getMessageForm(req, res) {
     res.render("messageForm");
 }
+
+
+const EMPTY_ERROR_MSG = "cannot be empty";
+const validateMessage = [
+    body("message").trim().notEmpty().withMessage(`Message ${EMPTY_ERROR_MSG}`),
+    body("username").trim().notEmpty().withMessage(`Username ${EMPTY_ERROR_MSG}`),
+]
+export const createMessage = [
+    validateMessage,
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).render("partials/errors", { errors: errors.array() });
+        }
+        const newMessage = {
+            message: req.body.message,
+            username: req.body.username,
+        };
+        await db.saveMessage(newMessage);
+        res.redirect('/');
+    },
+
+];
